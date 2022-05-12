c3b04924997f894aef0a10abd92cfa409c8a6eb1
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:00:28 +0900

    ✨- 리소스 프로바이더 생성 - 각 변수의 값 제공

---
+++ app/src/main/java/com/daon/goj_gam/util/provider/DefaultResourcesProvider.kt
@@ -0,0 +1,20 @@
+package com.daon.goj_gam.util.provider
+
+import android.content.Context
+import android.content.res.ColorStateList
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+class DefaultResourcesProvider(
+    private val context: Context
+): ResourcesProvider {
+
+    override fun getString(@StringRes resId: Int): String = context.getString(resId)
+
+    override fun getString(@StringRes resId: Int, vararg formArgs: Any): String = context.getString(resId, formArgs)
+
+    override fun getColor(@ColorRes resId: Int): Int = ContextCompat.getColor(context, resId)
+
+    override fun getColorStateList(@ColorRes resId: Int): ColorStateList = context.getColorStateList(resId)
+}
