20b8eae2e246b0e93dcf1fefe80b2d5167c72b94
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:01:44 +0900

    ✨- 뷰 홀더 파일 구성

---
+++ app/src/main/java/com/daon/goj_gam/widget/adapter/viewholder/EmptyViewHolder.kt
@@ -0,0 +1,20 @@
+package com.daon.goj_gam.widget.adapter.viewholder
+
+import com.daon.goj_gam.databinding.ViewholderEmptyBinding
+import com.daon.goj_gam.model.Model
+import com.daon.goj_gam.screen.base.BaseViewModel
+import com.daon.goj_gam.util.provider.ResourcesProvider
+import com.daon.goj_gam.widget.adapter.listener.AdapterListener
+
+class EmptyViewHolder(
+    binding: ViewholderEmptyBinding,
+    viewModel: BaseViewModel,
+    resourcesProvider: ResourcesProvider
+
+): ModelViewHolder<Model>(binding, viewModel, resourcesProvider) {
+
+    override fun reset() = Unit
+
+    override fun bindViews(model: Model, adapterListener: AdapterListener) = Unit
+
+}
