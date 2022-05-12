e8ce2b68bf4a6f743bf5557805408732f416d05e
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:12:35 +0900

    ✨- 모델 리사이클러 어댑터 생성

---
+++ app/src/main/java/com/daon/goj_gam/widget/adapter/ModelRecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.daon.goj_gam.widget.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.daon.goj_gam.model.CellType
+import com.daon.goj_gam.model.Model
+import com.daon.goj_gam.screen.base.BaseViewModel
+import com.daon.goj_gam.util.mapper.ModelViewHolderMapper
+import com.daon.goj_gam.util.provider.ResourcesProvider
+import com.daon.goj_gam.widget.adapter.listener.AdapterListener
+import com.daon.goj_gam.widget.adapter.viewholder.ModelViewHolder
+
+class ModelRecyclerAdapter<M: Model, VM: BaseViewModel>(
+    private var modelList: List<Model>,
+    private val viewModel: VM,
+    private val resourceProvider: ResourcesProvider,
+    private val adapterListener: AdapterListener
+): ListAdapter<Model, ModelViewHolder<M>>(Model.DIFF_CALLBACK) {
+
+    override fun getItemCount(): Int = modelList.size
+
+    override fun getItemViewType(position: Int): Int = modelList[position].type.ordinal
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder<M> {
+        return ModelViewHolderMapper.map(parent, CellType.values()[viewType], viewModel, resourceProvider)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onBindViewHolder(holder: ModelViewHolder<M>, position: Int) {
+        with(holder) {
+            bindData(modelList[position] as M)
+            bindViews(modelList[position] as M, adapterListener)
+        }
+    }
+
+    override fun submitList(list: MutableList<Model>?) {
+        list?.let { modelList = it }
+        super.submitList(list)
+    }
+}
