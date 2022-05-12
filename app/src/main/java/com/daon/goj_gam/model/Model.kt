8dddf34ade796d25d4ac2ae3f9255847c9d45456
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:11:39 +0900

    ✨- 모델 아이템 검사

---
+++ app/src/main/java/com/daon/goj_gam/model/Model.kt
@@ -0,0 +1,25 @@
+package com.daon.goj_gam.model
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.DiffUtil
+
+abstract class Model(
+    open val id: Long,
+    open val type: CellType
+) {
+
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<Model> = object  : DiffUtil.ItemCallback<Model>() {
+            override fun areItemsTheSame(oldItem: Model, newItem: Model): Boolean {
+                return oldItem.id == newItem.id && oldItem.type == newItem.type
+            }
+
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(oldItem: Model, newItem: Model): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
