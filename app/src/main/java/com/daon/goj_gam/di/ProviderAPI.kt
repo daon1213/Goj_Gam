5e0d7df27134db04915a11c09f33027a0d67be7d
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:16:26 +0900

    ✨- 레트로핏 활용한 API 제공 및 값 불러오기

---
+++ app/src/main/java/com/daon/goj_gam/di/ProviderAPI.kt
@@ -0,0 +1,36 @@
+package com.daon.goj_gam.di
+
+import com.daon.goj_gam.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+fun provideRetrofit(
+    okHttpClient: OkHttpClient,
+    gsonConverterFactory: GsonConverterFactory
+): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("")
+        .addConverterFactory(gsonConverterFactory)
+        .client(okHttpClient)
+        .build()
+}
+
+fun providerGsonConvertFactory(): GsonConverterFactory {
+    return GsonConverterFactory.create()
+}
+
+fun buildOkHttpClient(): OkHttpClient {
+    val interceptor = HttpLoggingInterceptor()
+    if (BuildConfig.DEBUG) {
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+    } else {
+        interceptor.level = HttpLoggingInterceptor.Level.NONE
+    }
+    return OkHttpClient.Builder()
+        .connectTimeout(5, TimeUnit.SECONDS)
+        .addInterceptor(interceptor)
+        .build()
+}
