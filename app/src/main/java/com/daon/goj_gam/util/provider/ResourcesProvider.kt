8e3d227bced150e2d18223eaf095fa359c9b98ab
daon1213 <diaeiyo1213@naver.com>
2022-05-12 11:17:38 +0900

    ✨- 리소스 getter()로 제공

---
+++ app/src/main/java/com/daon/goj_gam/util/provider/ResourcesProvider.kt
@@ -0,0 +1,17 @@
+package com.daon.goj_gam.util.provider
+
+import android.content.res.ColorStateList
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+
+interface ResourcesProvider {
+
+    fun getString(@StringRes resId: Int): String
+
+    fun getString(@StringRes resId: Int, vararg formArgs: Any): String
+
+    fun getColor(@ColorRes resId: Int):Int
+
+    fun getColorStateList(@ColorRes resId: Int): ColorStateList
+
+}
