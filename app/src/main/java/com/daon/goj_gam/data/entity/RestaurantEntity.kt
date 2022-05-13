8b49ba173f7ef78b7dcd047a5ac78dc9395f28db
daon1213 <diaeiyo1213@naver.com>
2022-05-13 09:06:49 +0900

    ✨- 식당 정보에 필요한 인스턴스 생성

---
+++ app/src/main/java/com/daon/goj_gam/data/entity/RestaurantEntity.kt
@@ -0,0 +1,18 @@
+package com.daon.goj_gam.data.entity
+
+import android.os.Parcelable
+import com.daon.goj_gam.screen.main.home.restaurant.RestaurantCategory
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class RestaurantEntity(
+    override val id: Long,
+    val restaurantCategory: RestaurantCategory,
+    val restaurantTitle: String,
+    val restaurantImageUrl: String,
+    val grade: Float,
+    val reviewCount: Int,
+    val deliveryTimeRange: Pair<Int, Int>,
+    val deliveryTipRange: Pair<Int, Int>,
+    val restaurantInfold: Int
+): Entity, Parcelable
