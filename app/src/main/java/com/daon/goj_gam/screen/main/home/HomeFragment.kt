a05b3a5e3174158f04898549e0c08e768b0e8621
daon1213 <diaeiyo1213@naver.com>
2022-05-13 09:03:42 +0900

    🎨 - 식당 정보 띄워주는 함수 추가

--- app/src/main/java/com/daon/goj_gam/screen/main/home/HomeFragment.kt
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/HomeFragment.kt
@@ -2,14 +2,46 @@ package com.daon.goj_gam.screen.main.home
 
 import com.daon.goj_gam.databinding.FragmentHomeBinding
 import com.daon.goj_gam.screen.base.BaseFragment
+import com.daon.goj_gam.screen.main.home.restaurant.RestaurantCategory
+import com.daon.goj_gam.screen.main.home.restaurant.RestaurantListFragment
+import com.daon.goj_gam.widget.adapter.RestaurantListFragmentPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class HomeFragment: BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
+
+
     override val viewModel by viewModel<HomeViewModel>()
 
     override fun getViewBinding(): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
+
+    private lateinit var viewPagerAdapter: RestaurantListFragmentPagerAdapter
+
+    override fun initViews() {
+        super.initViews()
+        initViewPager()
+    }
+
+    private fun initViewPager() = with(binding) {
+        val restaurantCategories = RestaurantCategory.values()
+
+        if (::viewPagerAdapter.isInitialized.not()){
+            val restaurantListFragment = restaurantCategories.map {
+                RestaurantListFragment.newInstance(it)
+            }
+            viewPagerAdapter = RestaurantListFragmentPagerAdapter(
+                this@HomeFragment,
+                restaurantListFragment
+            )
+            viewPager.adapter = viewPagerAdapter
+        }
+        viewPager.offscreenPageLimit = restaurantCategories.size
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setText(restaurantCategories[position].categoryNameId)
+        }.attach()
+    }
 
     override fun observeData() {
 
