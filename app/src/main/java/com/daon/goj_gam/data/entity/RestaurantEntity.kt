2f813642a0d3f0de8ea724f00be925525cd8a971
daon1213 <diaeiyo1213@naver.com>
2022-05-13 09:07:49 +0900

    ✨- Suppress 추가하여 인스턴스 충돌 방지

--- app/src/main/java/com/daon/goj_gam/data/entity/RestaurantEntity.kt
+++ app/src/main/java/com/daon/goj_gam/data/entity/RestaurantEntity.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.daon.goj_gam.screen.main.home.restaurant.RestaurantCategory
 import kotlinx.android.parcel.Parcelize
 
+@Suppress("DEPRECATED_ANNOTATION")
 @Parcelize
 data class RestaurantEntity(
     override val id: Long,
