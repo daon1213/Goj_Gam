b15b91ce3e9f63561e41d8d957ec5925c3fac3be
daon1213 <diaeiyo1213@naver.com>
2022-05-14 14:36:44 +0900

    🎨- BaseFragment 코드 개선

--- app/src/main/java/com/daon/goj_gam/screen/base/BaseFragment.kt
+++ app/src/main/java/com/daon/goj_gam/screen/base/BaseFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import kotlinx.coroutines.Job
@@ -30,11 +29,11 @@ abstract class BaseFragment<VM: BaseViewModel, VB: ViewBinding>: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initState()
+        this.initState()
     }
 
     open fun initState() {
-        arguments?.let {
+        arguments?.let { it: Bundle ->
             viewModel.storeState(it)
         }
         initViews()
