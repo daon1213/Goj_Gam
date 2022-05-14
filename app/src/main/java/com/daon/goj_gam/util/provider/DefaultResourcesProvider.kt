3b7a59828e84bb6973aee03a6b49b789a1f47622
daon1213 <diaeiyo1213@naver.com>
2022-05-14 14:37:49 +0900

    🎨- DefaultResourcesProvider 코드 개선

--- app/src/main/java/com/daon/goj_gam/util/provider/DefaultResourcesProvider.kt
+++ app/src/main/java/com/daon/goj_gam/util/provider/DefaultResourcesProvider.kt
@@ -12,7 +12,7 @@ class DefaultResourcesProvider(
 
     override fun getString(@StringRes resId: Int): String = context.getString(resId)
 
-    override fun getString(@StringRes resId: Int, vararg formArgs: Any): String = context.getString(resId, formArgs)
+    override fun getString(@StringRes resId: Int, vararg formArgs: Any): String = context.getString(resId, *formArgs)
 
     override fun getColor(@ColorRes resId: Int): Int = ContextCompat.getColor(context, resId)
 
