3c3b2d1cd7101fd8dd2a10ef3688d8beeb5e844a
daon1213 <diaeiyo1213@naver.com>
2022-05-14 14:43:18 +0900

    🎨- RestaurantCategory 및 adapterListener 추가

--- app/src/main/java/com/daon/goj_gam/screen/main/home/restaurant/RestaurantListFragment.kt
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/restaurant/RestaurantListFragment.kt
@@ -1,23 +1,47 @@
 package com.daon.goj_gam.screen.main.home.restaurant
 
-import android.view.View
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import com.daon.goj_gam.databinding.FragmentRestaurantListBinding
+import com.daon.goj_gam.model.restaurant.RestaurantModel
 import com.daon.goj_gam.screen.base.BaseFragment
+import com.daon.goj_gam.util.provider.ResourcesProvider
+import com.daon.goj_gam.widget.adapter.ModelRecyclerAdapter
+import com.daon.goj_gam.widget.adapter.listener.restaurant.RestaurantListListener
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
-class RestaurantListFragment: BaseFragment<RestaurantListViewModel, FragmentRestaurantListBinding>() {
+class RestaurantListFragment :
+    BaseFragment<RestaurantListViewModel, FragmentRestaurantListBinding>() {
 
     private val restaurantCategory by lazy { arguments?.getSerializable(RESTAURANT_CATEGORY_KEY) as RestaurantCategory }
 
     override val viewModel by viewModel<RestaurantListViewModel> { parametersOf(restaurantCategory) }
 
-    override fun getViewBinding(): FragmentRestaurantListBinding = FragmentRestaurantListBinding.inflate(layoutInflater)
+    override fun getViewBinding(): FragmentRestaurantListBinding =
+        FragmentRestaurantListBinding.inflate(layoutInflater)
+
+    private val resourcesProvider by inject<ResourcesProvider>()
+
+    private val adapter by lazy {
+        ModelRecyclerAdapter<RestaurantModel, RestaurantListViewModel>(listOf(),
+            viewModel,
+            resourcesProvider,
+            adapterListener = object : RestaurantListListener {
+                override fun onClickItem(model: RestaurantModel) {
+                    Toast.makeText(requireContext(), "$model", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+    }
+
+    override fun initViews() = with(binding) {
+        recyclerView.adapter = adapter
+    }
 
     override fun observeData() = viewModel.restaurantListLiveData.observe(viewLifecycleOwner) {
-
-
+        adapter.submitList(it)
     }
 
     companion object {
