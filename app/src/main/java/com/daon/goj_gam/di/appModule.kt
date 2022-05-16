f937e68bb698cda9d4a10ebb4902c53c6413bc70
daon1213 <diaeiyo1213@naver.com>
2022-05-17 07:42:54 +0900

    🎨- 뷰모델 리턴 코드 수정

--- app/src/main/java/com/daon/goj_gam/di/appModule.kt
+++ app/src/main/java/com/daon/goj_gam/di/appModule.kt
@@ -1,13 +1,16 @@
 package com.daon.goj_gam.di
 
 
-import com.daon.goj_gam.data.repository.DefaultRestaurantRepository
-import com.daon.goj_gam.data.repository.RestaurantRepository
+import com.daon.goj_gam.data.repository.map.DefaultMapRepository
+import com.daon.goj_gam.data.repository.map.MapRepository
+import com.daon.goj_gam.data.repository.restaurant.DefaultRestaurantRepository
+import com.daon.goj_gam.data.repository.restaurant.RestaurantRepository
 import com.daon.goj_gam.screen.main.home.HomeViewModel
 import com.daon.goj_gam.screen.main.home.restaurant.RestaurantCategory
 import com.daon.goj_gam.screen.main.home.restaurant.RestaurantListViewModel
 import com.daon.goj_gam.screen.main.my.MyViewModel
 import com.daon.goj_gam.util.provider.DefaultResourcesProvider
+import com.daon.goj_gam.util.provider.ResourcesProvider
 import kotlinx.coroutines.Dispatchers
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -15,18 +18,19 @@ import org.koin.dsl.module
 
 val appModule = module {
 
-    viewModel { return@viewModel HomeViewModel() }
-    viewModel { return@viewModel MyViewModel() }
+    viewModel { HomeViewModel() }
+    viewModel { MyViewModel() }
     viewModel { (restaurantCategory: RestaurantCategory) -> RestaurantListViewModel(restaurantCategory, get()) }
 
-    single<RestaurantRepository> { DefaultRestaurantRepository(get(), get()) }
+    single <RestaurantRepository> { DefaultRestaurantRepository(get(), get()) }
+    single <MapRepository> { DefaultMapRepository(get(), get()) }
 
     single { providerGsonConvertFactory() }
     single { buildOkHttpClient() }
 
     single { provideRetrofit(get(), get()) }
 
-    single<com.daon.goj_gam.util.provider.ResourcesProvider> {DefaultResourcesProvider(androidApplication())}
+    single<ResourcesProvider> {DefaultResourcesProvider(androidApplication())}
 
     single { Dispatchers.IO }
     single { Dispatchers.Main }
