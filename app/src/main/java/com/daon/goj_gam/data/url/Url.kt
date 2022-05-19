88478691f31c305609bc7095c99ea3c8413cb941
daon1213 <diaeiyo1213@naver.com>
2022-05-20 08:55:21 +0900

    ✨ - 데이터를 가져오는 Url 명시

---
+++ app/src/main/java/com/daon/goj_gam/data/url/Url.kt
@@ -0,0 +1,10 @@
+package com.daon.goj_gam.data.url
+
+object Url {
+    const val TMAP_URL = "https://apis.openapi.sk.com"
+    const val GET_TMAP_POIS = "/tmap/pois"
+    const val GET_TMAP_POIS_AROUND = "/tmap/pois/search/around"
+    const val GET_TMAP_REVERSE_GEO_CODE = "/tmap/geo/reversegeocoding"
+
+    const val FOOD_URL = "https://60abc8f15a4de40017ccae3e.mockapi.io/"
+}
