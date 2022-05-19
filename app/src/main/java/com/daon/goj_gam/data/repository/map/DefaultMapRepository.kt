6984f7f9a3c6a7bf540e3682c0238e2b5155b9c9
daon1213 <diaeiyo1213@naver.com>
2022-05-20 08:52:22 +0900

    ✨ - 지도에서 주소 정보 가져오기

---
+++ app/src/main/java/com/daon/goj_gam/data/repository/map/DefaultMapRepository.kt
@@ -0,0 +1,28 @@
+package com.daon.goj_gam.data.repository.map
+
+import com.daon.goj_gam.data.entity.LocationLatLngEntity
+import com.daon.goj_gam.data.network.MapApiService
+import com.daon.goj_gam.data.reponse.address.AddressInfo
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class DefaultMapRepository(
+    private val mapApiService: MapApiService,
+    private val ioDispatcher: CoroutineDispatcher
+) : MapRepository {
+
+    override suspend fun getReverseGeoInformation(
+        locationLatLngEntity: LocationLatLngEntity
+    ): AddressInfo? = withContext(ioDispatcher) {
+        val response = mapApiService.getReverseGeoCode(
+            lat = locationLatLngEntity.latitude,
+            lon = locationLatLngEntity.longitude
+        )
+        if (response.isSuccessful) {
+            response.body()?.addressInfo
+        } else {
+            null
+        }
+    }
+
+}
