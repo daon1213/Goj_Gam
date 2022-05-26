b7a648d630d5b4b4b12db9625ffabde7b744937c
daon1213 <diaeiyo1213@naver.com>
2022-05-27 08:39:56 +0900

    🔧 - HomeViewModel 구조 수정

--- app/src/main/java/com/daon/goj_gam/screen/main/home/HomeViewModel.kt
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/HomeViewModel.kt
@@ -1,6 +1,47 @@
 package com.daon.goj_gam.screen.main.home
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.daon.goj_gam.R
+import com.daon.goj_gam.data.entity.LocationLatLngEntity
+import com.daon.goj_gam.data.entity.MapSearchInfoEntity
+import com.daon.goj_gam.data.repository.map.MapRepository
 import com.daon.goj_gam.screen.base.BaseViewModel
+import kotlinx.coroutines.launch
 
-class HomeViewModel:BaseViewModel() {
+class HomeViewModel(
+    private val mapRepository: MapRepository
+):BaseViewModel() {
+
+    companion object {
+        const val MY_LOCATION_KEY = "MyLocation"
+    }
+
+    val homeStateLiveData = MutableLiveData<HomeState>(HomeState.Uninitialized)
+
+    fun loadReverseGeoInformation(
+        locationLatLngEntity: LocationLatLngEntity
+    ) = viewModelScope.launch {
+        homeStateLiveData.value = HomeState.Loading
+        val addressInfo = mapRepository.getReverseGeoInformation(locationLatLngEntity)
+        addressInfo?.let { info ->
+            homeStateLiveData.value = HomeState.Success(
+                mapSearchInfo =  info.toSearchInfoEntity(locationLatLngEntity)
+            )
+        } ?: kotlin.run {
+            homeStateLiveData.value = HomeState.Error(
+                R.string.fail_to_load_address_info
+            )
+        }
+    }
+
+    fun getMapSearchInfo(): MapSearchInfoEntity? {
+        when (val data = homeStateLiveData.value) {
+            is HomeState.Success -> {
+                return data.mapSearchInfo
+            }
+        }
+        return null
+    }
+
 }
