6069d50c254a1744b2e89ca7fa0ee3cb3571db17
daon1213 <diaeiyo1213@naver.com>
2022-05-27 08:40:55 +0900

    🔧 - MyFragment 구조 수정

--- app/src/main/java/com/daon/goj_gam/screen/main/my/MyFragment.kt
+++ app/src/main/java/com/daon/goj_gam/screen/main/my/MyFragment.kt
@@ -1,10 +1,7 @@
 package com.daon.goj_gam.screen.main.my
 
-import com.daon.goj_gam.databinding.FragmentHomeBinding
 import com.daon.goj_gam.databinding.FragmentMyBinding
 import com.daon.goj_gam.screen.base.BaseFragment
-import com.daon.goj_gam.screen.main.home.HomeFragment
-import com.daon.goj_gam.screen.main.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
