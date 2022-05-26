3b1cffff0ad6f47b7f0c707c388b34119287356d
daon1213 <diaeiyo1213@naver.com>
2022-05-26 11:29:43 +0900

    ✨ - Tmap API 불러오기

---
+++ app/src/main/java/com/daon/goj_gam/data/network/MapApiService.kt
@@ -0,0 +1,65 @@
+package com.daon.goj_gam.data.network
+
+import com.daon.goj_gam.data.reponse.address.AddressInfoResponse
+import com.daon.goj_gam.data.reponse.search.SearchResponse
+import com.daon.goj_gam.data.url.Key
+import com.daon.goj_gam.data.url.Url
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface MapApiService {
+
+
+    @GET(Url.GET_TMAP_POIS)
+    suspend fun getSearchLocation(
+        @Header("appKey") appKey: String = Key.TMAP_API,
+        @Query("version") version: Int = 1,
+        @Query("callback") callback: String? = null,
+        @Query("count") count: Int = 20,
+        @Query("searchKeyword") keyword: String? = null,
+        @Query("areaLLCode") areaLLCode: String? = null,
+        @Query("areaLMCode") areaLMCode: String? = null,
+        @Query("resCoordType") resCoordType: String? = null,
+        @Query("searchType") searchType: String? = null,
+        @Query("multiPoint") multiPoint: String? = null,
+        @Query("searchtypCd") searchtypCd: String? = null,
+        @Query("radius") radius: String? = null,
+        @Query("reqCoordType") reqCoordType: String? = null,
+        @Query("centerLon") centerLon: String? = null,
+        @Query("centerLat") centerLat: String? = null
+    ): Response<SearchResponse>
+
+    @GET(Url.GET_TMAP_POIS_AROUND)
+    suspend fun getSearchLocationAround(
+    @Header("appKey") appKey: String = Key.TMAP_API,
+    @Query("version") version: Int = 1,
+    @Query("categories") categories: String? = null,
+    @Query("callback") callback: String? = null,
+    @Query("count") count: Int = 20,
+    @Query("searchKeyword") keyword: String? = null,
+    @Query("areaLLCode") areaLLCode: String? = null,
+    @Query("areaLMCode") areaLMCode: String? = null,
+    @Query("resCoordType") resCoordType: String? = null,
+    @Query("searchType") searchType: String? = null,
+    @Query("multiPoint") multiPoint: String? = null,
+    @Query("searchtypCd") searchtypCd: String? = null,
+    @Query("radius") radius: String? = null,
+    @Query("reqCoordType") reqCoordType: String? = null,
+    @Query("centerLon") centerLon: String? = null,
+    @Query("centerLat") centerLat: String? = null
+    ): Response<SearchResponse>
+
+    @GET(Url.GET_TMAP_REVERSE_GEO_CODE)
+    suspend fun getReverseGeoCode(
+        @Header("appKey") appKey: String = Key.TMAP_API,
+        @Query("version") version: Int = 1,
+        @Query("callback") callback: String? = null,
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("coordType") coordType: String? = null,
+        @Query("addressType") addressType: String? = null
+    ): Response<AddressInfoResponse>
+
+}
