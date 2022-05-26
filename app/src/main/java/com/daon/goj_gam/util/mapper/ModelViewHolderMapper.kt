7bd3c4a196fceac5873a73f7e35cff8b949e3338
daon1213 <diaeiyo1213@naver.com>
2022-05-27 08:40:35 +0900

    🔧 - ModelViewHolderMapper 구조 수정

--- app/src/main/java/com/daon/goj_gam/util/mapper/ModelViewHolderMapper.kt
+++ app/src/main/java/com/daon/goj_gam/util/mapper/ModelViewHolderMapper.kt
@@ -2,14 +2,17 @@ package com.daon.goj_gam.util.mapper
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.ViewParent
 import com.daon.goj_gam.databinding.ViewholderEmptyBinding
+import com.daon.goj_gam.databinding.ViewholderRestaurantBinding
 import com.daon.goj_gam.model.CellType
+import com.daon.goj_gam.model.CellType.EMPTY_CELL
+import com.daon.goj_gam.model.CellType.RESTAURANT_CELL
 import com.daon.goj_gam.model.Model
 import com.daon.goj_gam.screen.base.BaseViewModel
 import com.daon.goj_gam.util.provider.ResourcesProvider
 import com.daon.goj_gam.widget.adapter.viewholder.EmptyViewHolder
 import com.daon.goj_gam.widget.adapter.viewholder.ModelViewHolder
+import com.daon.goj_gam.widget.adapter.viewholder.restaurant.RestaurantViewHolder
 
 object ModelViewHolderMapper {
 
@@ -22,8 +25,13 @@ object ModelViewHolderMapper {
     ): ModelViewHolder<M> {
         val inflater = LayoutInflater.from(parent.context)
         val viewHolder = when(type) {
-            CellType.EMPTY_CELL -> EmptyViewHolder(
+            EMPTY_CELL -> EmptyViewHolder(
                 ViewholderEmptyBinding.inflate(inflater, parent, false),
+                viewModel,
+                resourcesProvider
+            )
+            RESTAURANT_CELL -> RestaurantViewHolder(
+                ViewholderRestaurantBinding.inflate(inflater, parent, false),
                 viewModel,
                 resourcesProvider
             )
