1a4fdd11bfd1ee98c7cd413db2c2e58abe6c7945
daon1213 <diaeiyo1213@naver.com>
2022-06-02 17:05:11 +0900

    🔧 -  getSearchLocation 함수 주석처리

--- app/src/main/java/com/daon/goj_gam/data/network/MapApiService.kt
+++ app/src/main/java/com/daon/goj_gam/data/network/MapApiService.kt
@@ -12,24 +12,24 @@ import retrofit2.http.Query
 interface MapApiService {
 
 
-    @GET(Url.GET_TMAP_POIS)
-    suspend fun getSearchLocation(
-        @Header("appKey") appKey: String = Key.TMAP_API,
-        @Query("version") version: Int = 1,
-        @Query("callback") callback: String? = null,
-        @Query("count") count: Int = 20,
-        @Query("searchKeyword") keyword: String? = null,
-        @Query("areaLLCode") areaLLCode: String? = null,
-        @Query("areaLMCode") areaLMCode: String? = null,
-        @Query("resCoordType") resCoordType: String? = null,
-        @Query("searchType") searchType: String? = null,
-        @Query("multiPoint") multiPoint: String? = null,
-        @Query("searchtypCd") searchtypCd: String? = null,
-        @Query("radius") radius: String? = null,
-        @Query("reqCoordType") reqCoordType: String? = null,
-        @Query("centerLon") centerLon: String? = null,
-        @Query("centerLat") centerLat: String? = null
-    ): Response<SearchResponse>
+//    @GET(Url.GET_TMAP_POIS)
+//    suspend fun getSearchLocation(
+//        @Header("appKey") appKey: String = Key.TMAP_API,
+//        @Query("version") version: Int = 1,
+//        @Query("callback") callback: String? = null,
+//        @Query("count") count: Int = 20,
+//        @Query("searchKeyword") keyword: String? = null,
+//        @Query("areaLLCode") areaLLCode: String? = null,
+//        @Query("areaLMCode") areaLMCode: String? = null,
+//        @Query("resCoordType") resCoordType: String? = null,
+//        @Query("searchType") searchType: String? = null,
+//        @Query("multiPoint") multiPoint: String? = null,
+//        @Query("searchtypCd") searchtypCd: String? = null,
+//        @Query("radius") radius: String? = null,
+//        @Query("reqCoordType") reqCoordType: String? = null,
+//        @Query("centerLon") centerLon: String? = null,
+//        @Query("centerLat") centerLat: String? = null
+//    ): Response<SearchResponse>
 
     @GET(Url.GET_TMAP_POIS_AROUND)
     suspend fun getSearchLocationAround(
