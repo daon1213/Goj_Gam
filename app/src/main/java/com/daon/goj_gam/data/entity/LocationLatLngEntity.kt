79122fe07ca4f59940474b7ead13b8af80a53cfc
daon1213 <diaeiyo1213@naver.com>
2022-06-10 11:37:54 +0900

    🔧 - Primary Key 추가

--- app/src/main/java/com/daon/goj_gam/data/entity/LocationLatLngEntity.kt
+++ app/src/main/java/com/daon/goj_gam/data/entity/LocationLatLngEntity.kt
@@ -1,11 +1,14 @@
 package com.daon.goj_gam.data.entity
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@androidx.room.Entity
 data class LocationLatLngEntity (
     val latitude: Double,
     val longitude: Double,
+    @PrimaryKey(autoGenerate = true)
     override val id: Long = -1
 ): Entity, Parcelable
