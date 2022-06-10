2f3935bb56c6a7488b98fbac4c98b77a4e7387b7
daon1213 <diaeiyo1213@naver.com>
2022-06-10 11:35:35 +0900

    🔧 - UserLiked 함수 3개 추가

--- app/src/main/java/com/daon/goj_gam/data/repository/user/DefaultUserRepository.kt
+++ app/src/main/java/com/daon/goj_gam/data/repository/user/DefaultUserRepository.kt
@@ -1,12 +1,15 @@
 package com.daon.goj_gam.data.repository.user
 
 import com.daon.goj_gam.data.db.dao.LocationDao
+import com.daon.goj_gam.data.db.dao.RestaurantDao
 import com.daon.goj_gam.data.entity.LocationLatLngEntity
+import com.daon.goj_gam.data.entity.RestaurantEntity
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
 class DefaultUserRepository(
     private val locationDao: LocationDao,
+    private val restaurantDao: RestaurantDao,
     private val ioDispatcher: CoroutineDispatcher
 ): UserRepository {
 
@@ -15,5 +18,21 @@ class DefaultUserRepository(
     }
     override suspend fun insertUserLocation(locationLatLngEntity: LocationLatLngEntity) = withContext(ioDispatcher) {
         locationDao.insert(locationLatLngEntity)
+    }
+
+    override suspend fun getUserLikedRestaurant(restaurantTitle: String): RestaurantEntity? = withContext(ioDispatcher) {
+        restaurantDao.get(restaurantTitle)
+    }
+
+    override suspend fun insertUserLikedRestaurant(restaurantEntity: RestaurantEntity) = withContext(ioDispatcher) {
+        restaurantDao.insert(restaurantEntity)
+    }
+
+    override suspend fun deleteUserLikedRestaurant(restaurantTitle: String) {
+        restaurantDao.delete(restaurantTitle)
+    }
+
+    override suspend fun deleteAllUserLikedRestaurant() {
+        restaurantDao.deleteAll()
     }
 }
