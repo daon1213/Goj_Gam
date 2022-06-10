6a3f6e539df42a484d74d1f3c9887b1b29fbcdc4
daon1213 <diaeiyo1213@naver.com>
2022-06-10 11:39:05 +0900

    🔧 - MY LOCATION KEY 생성

--- app/src/main/java/com/daon/goj_gam/screen/main/home/HomeViewModel.kt
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/HomeViewModel.kt
@@ -6,11 +6,13 @@ import com.daon.goj_gam.R
 import com.daon.goj_gam.data.entity.LocationLatLngEntity
 import com.daon.goj_gam.data.entity.MapSearchInfoEntity
 import com.daon.goj_gam.data.repository.map.MapRepository
+import com.daon.goj_gam.data.repository.user.UserRepository
 import com.daon.goj_gam.screen.base.BaseViewModel
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
-    private val mapRepository: MapRepository
+    private val mapRepository: MapRepository,
+    private val userRepository: UserRepository
 ):BaseViewModel() {
 
     companion object {
@@ -23,10 +25,14 @@ class HomeViewModel(
         locationLatLngEntity: LocationLatLngEntity
     ) = viewModelScope.launch {
         homeStateLiveData.value = HomeState.Loading
-        val addressInfo = mapRepository.getReverseGeoInformation(locationLatLngEntity)
+        val userLocation = userRepository.getUserLocation()
+        val currentLocation = userLocation ?: locationLatLngEntity
+
+        val addressInfo = mapRepository.getReverseGeoInformation(currentLocation)
         addressInfo?.let { info ->
             homeStateLiveData.value = HomeState.Success(
-                mapSearchInfo =  info.toSearchInfoEntity(locationLatLngEntity)
+                mapSearchInfo =  info.toSearchInfoEntity(locationLatLngEntity),
+                isLocationSame = currentLocation == locationLatLngEntity
             )
         } ?: kotlin.run {
             homeStateLiveData.value = HomeState.Error(
