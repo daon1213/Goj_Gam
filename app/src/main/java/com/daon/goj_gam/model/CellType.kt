a122c8999f57523a20188c96b2299c6749c85016
daon1213 <diaeiyo1213@naver.com>
2022-06-20 12:02:35 +0900

    🔧 - Enum class로 CellType 활용 속성 생성

--- app/src/main/java/com/daon/goj_gam/model/CellType.kt
+++ app/src/main/java/com/daon/goj_gam/model/CellType.kt
@@ -3,6 +3,11 @@ package com.daon.goj_gam.model
 enum class CellType {
 
     EMPTY_CELL,
-    RESTAURANT_CELL
+    RESTAURANT_CELL,
+    LIKED_RESTAURANT_CELL,
+    FOOD_CELL,
+    REVIEW_CELL,
+    ORDER_FOOD_CELL,
+    ORDER_CELL
 
 }
