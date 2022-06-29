b6da2712caa132779772d5ff092bbbcdeb29e41b
daon1213 <diaeiyo1213@naver.com>
2022-06-29 13:23:34 +0900

    🔧 - Food API 제공 추가

--- app/src/main/java/com/daon/goj_gam/di/ProviderAPI.kt
+++ app/src/main/java/com/daon/goj_gam/di/ProviderAPI.kt
@@ -1,6 +1,7 @@
 package com.daon.goj_gam.di
 
 import com.daon.goj_gam.BuildConfig
+import com.daon.goj_gam.data.network.FoodApiService
 import com.daon.goj_gam.data.network.MapApiService
 import com.daon.goj_gam.data.url.Url
 import okhttp3.OkHttpClient
@@ -13,12 +14,27 @@ fun provideMapApiService(retrofit: Retrofit): MapApiService {
     return retrofit.create(MapApiService::class.java)
 }
 
-fun provideRetrofit(
+fun provideFoodApiService(retrofit: Retrofit): FoodApiService {
+    return retrofit.create(FoodApiService::class.java)
+}
+
+fun provideMapRetrofit(
     okHttpClient: OkHttpClient,
     gsonConverterFactory: GsonConverterFactory
 ): Retrofit {
     return Retrofit.Builder()
         .baseUrl(Url.TMAP_URL)
+        .addConverterFactory(gsonConverterFactory)
+        .client(okHttpClient)
+        .build()
+}
+
+fun provideFoodRetrofit(
+    okHttpClient: OkHttpClient,
+    gsonConverterFactory: GsonConverterFactory
+): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(Url.FOOD_URL)
         .addConverterFactory(gsonConverterFactory)
         .client(okHttpClient)
         .build()
