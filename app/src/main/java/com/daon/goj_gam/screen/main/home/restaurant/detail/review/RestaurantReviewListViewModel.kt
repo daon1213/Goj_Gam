c7194b93c0f9169bbddb3be745c51f4dccf11e2b
daon1213 <diaeiyo1213@naver.com>
2022-07-06 10:24:12 +0900

    🔧 - Review ViewModel 구현

---
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/restaurant/detail/review/RestaurantReviewListViewModel.kt
@@ -0,0 +1,51 @@
+package com.daon.goj_gam.screen.main.home.restaurant.detail.review
+
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.daon.goj_gam.data.entity.impl.ReviewEntity
+import com.daon.goj_gam.data.repository.review.DefaultRestaurantReviewRepository
+import com.daon.goj_gam.data.repository.review.RestaurantReviewRepository
+import com.daon.goj_gam.model.review.ReviewModel
+import com.daon.goj_gam.screen.base.BaseViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class RestaurantReviewListViewModel(
+    private val restaurantTitle: String,
+    private val restaurantReviewRepository: RestaurantReviewRepository
+): BaseViewModel() {
+
+    private val _reviewStateLiveData = MutableLiveData<RestaurantReviewState>(RestaurantReviewState.UnInitialized)
+    val reviewStateLiveDate
+        get() = _reviewStateLiveData
+
+    override fun fetchData(): Job = viewModelScope.launch{
+        _reviewStateLiveData.value = RestaurantReviewState.Loading
+        when(val reviews = restaurantReviewRepository.getReviews(restaurantTitle)) {
+            is DefaultRestaurantReviewRepository.Result.Error -> {
+                _reviewStateLiveData.value = RestaurantReviewState.Error(
+                    reviews.e
+                )
+            }
+            is DefaultRestaurantReviewRepository.Result.Success<*> -> {
+                _reviewStateLiveData.value = RestaurantReviewState.Success(
+                    (reviews.data as List<ReviewEntity>).map {
+                        ReviewModel(
+                            id = it.id,
+                            title = it.title,
+                            description = it.content,
+                            grade = it.rating.toInt(),
+                            thumbnailImageUri = if (it.imagesUrlList?.isNotEmpty() ?: false) {
+                                Uri.parse(it.imagesUrlList?.first())
+                            } else {
+                                null
+                            }
+                        )
+                    }
+                )
+            }
+        }
+    }
+
+}
