762f87b666b025c4ec3123f184036e4096e66b94
daon1213 <diaeiyo1213@naver.com>
2022-07-19 08:19:01 +0900

    🔧 - 상위 ViewModel 접근 및 기능 구현 필요 함수 정의

---
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/restaurant/detail/menu/RestaurantMenuListFragment.kt
@@ -0,0 +1,93 @@
+package com.daon.goj_gam.screen.main.home.restaurant.detail.menu
+
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import com.daon.goj_gam.data.entity.impl.RestaurantFoodEntity
+import com.daon.goj_gam.databinding.FragmentListBinding
+import com.daon.goj_gam.model.food.FoodModel
+import com.daon.goj_gam.screen.base.BaseFragment
+import com.daon.goj_gam.screen.main.home.restaurant.detail.RestaurantDetailViewModel
+import com.daon.goj_gam.util.provider.ResourcesProvider
+import com.daon.goj_gam.widget.adapter.ModelRecyclerAdapter
+import com.daon.goj_gam.widget.adapter.listener.food.FoodMenuListListener
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+class RestaurantMenuListFragment :
+    BaseFragment<RestaurantMenuListViewModel, FragmentListBinding>() {
+
+    private val resourceProvider by inject<ResourcesProvider>()
+
+    private val restaurantId by lazy { arguments?.getLong(RESTAURANT_ID_KEY, -1) }
+    private val restauranFoodList by lazy {
+        arguments?.getParcelableArrayList<RestaurantFoodEntity>(
+            FOOD_LIST_KEY
+        )!!
+    }
+    override val viewModel: RestaurantMenuListViewModel by viewModel {
+        parametersOf(
+            restaurantId,
+            restauranFoodList
+        )
+    }
+    // 상위 viewModel 을 koin 으로 접근
+    private val restaurantDetailViewModel by sharedViewModel<RestaurantDetailViewModel>()
+
+    override fun getViewBinding(): FragmentListBinding =
+        FragmentListBinding.inflate(layoutInflater)
+
+    private val adapter by lazy {
+        ModelRecyclerAdapter<FoodModel, RestaurantMenuListViewModel>(
+            listOf(),
+            viewModel,
+            resourceProvider,
+            adapterListener = object : FoodMenuListListener {
+                override fun onClickItem(model: FoodModel) {
+                    viewModel.insertMenuInBasket(model)
+                }
+            }
+        )
+    }
+
+    override fun initViews() = with(binding) {
+        super.initViews()
+        recyclerView.adapter = adapter
+    }
+
+    override fun observeData() {
+        viewModel.foodListLiveData.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+        viewModel.menuBasketLiveData.observe(viewLifecycleOwner) {
+            // 장바구니에 메뉴가 담긴 경우
+            Toast.makeText(requireContext(), "${it.title}이 장바구니에 담겼습니다.", Toast.LENGTH_SHORT).show()
+            restaurantDetailViewModel.notifyFoodMenuListInBasket(it)
+        }
+        viewModel.isClearNeedInBasketLiveData.observe(viewLifecycleOwner) { (isClearNeed, cb) ->
+            if (isClearNeed) {
+                restaurantDetailViewModel.notifyClearNeedAlertInBasket(isClearNeed, cb)
+            }
+        }
+    }
+
+    companion object {
+
+        const val RESTAURANT_ID_KEY = "restaurantId"
+        const val FOOD_LIST_KEY = "foodList"
+
+        fun newInstance(
+            restaurantId: Long,
+            foodList: ArrayList<RestaurantFoodEntity>
+        ): RestaurantMenuListFragment {
+            val bundle = bundleOf(
+                RESTAURANT_ID_KEY to restaurantId,
+                FOOD_LIST_KEY to foodList
+            )
+            return RestaurantMenuListFragment().apply {
+                arguments = bundle
+            }
+        }
+    }
+}
