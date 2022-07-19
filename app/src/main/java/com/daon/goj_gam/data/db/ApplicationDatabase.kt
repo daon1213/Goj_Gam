573c3fa47f92cbde9b9f7d3ec000f3c43f182c80
daon1213 <diaeiyo1213@naver.com>
2022-07-20 08:14:17 +0900

    🔧 - 패키지 변경에 의한 import 변경 및 장바구니 함수 추가

--- app/src/main/java/com/daon/goj_gam/data/db/ApplicationDatabase.kt
+++ app/src/main/java/com/daon/goj_gam/data/db/ApplicationDatabase.kt
@@ -2,13 +2,15 @@ package com.daon.goj_gam.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.daon.goj_gam.data.db.dao.FoodMenuBasketDao
 import com.daon.goj_gam.data.db.dao.LocationDao
 import com.daon.goj_gam.data.db.dao.RestaurantDao
 import com.daon.goj_gam.data.entity.impl.LocationLatLngEntity
 import com.daon.goj_gam.data.entity.impl.RestaurantEntity
+import com.daon.goj_gam.data.entity.impl.RestaurantFoodEntity
 
 @Database(
-    entities = [LocationLatLngEntity::class, RestaurantEntity::class],
+    entities = [LocationLatLngEntity::class, RestaurantEntity::class, RestaurantFoodEntity::class],
     version = 1,
     exportSchema = false
 )
@@ -22,4 +24,5 @@ abstract class ApplicationDatabase: RoomDatabase() {
 
     abstract fun RestaurantDao(): RestaurantDao
 
+    abstract fun FoodMenuBasketDao(): FoodMenuBasketDao
 }
