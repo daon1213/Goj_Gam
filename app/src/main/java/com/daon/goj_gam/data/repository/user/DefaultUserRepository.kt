74c9bc7a4c8b17e69d7e905d732e7732152ddb1f
daon1213 <diaeiyo1213@naver.com>
2022-07-24 14:31:35 +0900

    🔧 - 패키지 변경에 의한 import 변경 및 찜 기능 함수 추가

--- app/src/main/java/com/daon/goj_gam/data/repository/user/DefaultUserRepository.kt
+++ app/src/main/java/com/daon/goj_gam/data/repository/user/DefaultUserRepository.kt
@@ -2,8 +2,8 @@ package com.daon.goj_gam.data.repository.user
 
 import com.daon.goj_gam.data.db.dao.LocationDao
 import com.daon.goj_gam.data.db.dao.RestaurantDao
-import com.daon.goj_gam.data.entity.LocationLatLngEntity
-import com.daon.goj_gam.data.entity.RestaurantEntity
+import com.daon.goj_gam.data.entity.impl.LocationLatLngEntity
+import com.daon.goj_gam.data.entity.impl.RestaurantEntity
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
@@ -29,10 +29,16 @@ class DefaultUserRepository(
     }
 
     override suspend fun deleteUserLikedRestaurant(restaurantTitle: String) {
-        restaurantDao.delete(restaurantTitle)
+        restaurantDao.delete(restaurantTitle.toString())
     }
 
     override suspend fun deleteAllUserLikedRestaurant() {
         restaurantDao.deleteAll()
     }
+
+    override suspend fun getAllUserLikedRestaurant(): List<RestaurantEntity> =
+        withContext(ioDispatcher) {
+            restaurantDao.getAll()
+        }
+
 }
