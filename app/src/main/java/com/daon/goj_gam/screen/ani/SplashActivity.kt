8e62f4fea6676d9a5ca0c158257a69601c11383f
daon1213 <diaeiyo1213@naver.com>
2022-08-12 14:56:45 +0900

    🔧 - Lottie Delivery 애니메이션 화면 구현

---
+++ app/src/main/java/com/daon/goj_gam/screen/ani/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.daon.goj_gam.screen.ani
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.daon.goj_gam.R
+import com.daon.goj_gam.screen.main.MainActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        var lodingImage = findViewById(R.id.loading_image) as LottieAnimationView
+
+        // 애니메이션 시작
+        lodingImage.playAnimation()
+
+        val handler: Handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2500)
+    }
+}
