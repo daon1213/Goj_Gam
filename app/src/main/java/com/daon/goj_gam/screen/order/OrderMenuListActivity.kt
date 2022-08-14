c5cc004f20b7dbfc51e14f55d9376d76bfb62369
daon1213 <diaeiyo1213@naver.com>
2022-08-14 16:36:12 +0900

    🔧 - 장바구니 클릭 or 주문 내역 확인 및 주문 하기 기능 완성

---
+++ app/src/main/java/com/daon/goj_gam/screen/order/OrderMenuListActivity.kt
@@ -0,0 +1,94 @@
+package com.daon.goj_gam.screen.order
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.daon.goj_gam.databinding.ActivityOrderListBinding
+import com.daon.goj_gam.model.food.FoodModel
+import com.daon.goj_gam.screen.base.BaseActivity
+import com.daon.goj_gam.util.provider.ResourcesProvider
+import com.daon.goj_gam.widget.adapter.ModelRecyclerAdapter
+import com.daon.goj_gam.widget.adapter.listener.order.OrderMenuListListener
+import org.koin.android.ext.android.inject
+
+class OrderMenuListActivity :
+    BaseActivity<OrderMenuListViewModel, ActivityOrderListBinding>() {
+
+    override val viewModel: OrderMenuListViewModel by inject()
+    private val resourcesProvider by inject<ResourcesProvider>()
+
+    override fun getViewBinding(): ActivityOrderListBinding =
+        ActivityOrderListBinding.inflate(layoutInflater)
+
+    private val adapter by lazy {
+        ModelRecyclerAdapter<FoodModel, OrderMenuListViewModel>(
+            listOf(),
+            viewModel,
+            resourcesProvider,
+            adapterListener = object : OrderMenuListListener {
+                override fun onRemoveItem(foodModel: FoodModel) {
+                    viewModel.removeOrderMenu(foodModel)
+                }
+            }
+        )
+    }
+
+    override fun initViews() = with(binding) {
+        recyclerVIew.adapter = adapter
+        toolbar.setNavigationOnClickListener { finish() }
+        confirmButton.setOnClickListener {
+            viewModel.orderMenu()
+        }
+        orderClearButton.setOnClickListener {
+            viewModel.clearOrderMenu()
+        }
+    }
+
+    override fun observeData() = viewModel.orderStateLiveData.observe(this) {
+        when (it) {
+            is OrderMenuState.Error -> handleErrorState(it)
+            OrderMenuState.Loading -> handleLoadingState()
+            OrderMenuState.Order -> handleOrderState()
+            is OrderMenuState.Success -> handleSuccessState(it)
+            OrderMenuState.UnInitialized -> Unit
+        }
+    }
+
+    private fun handleErrorState(state: OrderMenuState.Error) {
+        Toast.makeText(this, getString(state.messageId), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleLoadingState() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun handleSuccessState(state: OrderMenuState.Success) = with(binding) {
+        progressBar.isGone = true
+
+        val menuOrderIsEmpty = state.restaurantFoodModelIst?.isEmpty() ?: true
+        confirmButton.isEnabled = menuOrderIsEmpty.not()
+        if (menuOrderIsEmpty) {
+            Toast.makeText(
+                this@OrderMenuListActivity,
+                "주문 내용이 없어 화면을 종료합니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        } else {
+            adapter.submitList(state.restaurantFoodModelIst)
+        }
+    }
+
+    private fun handleOrderState() {
+        // 주문이 완료된 경우
+        Toast.makeText(this, "성공적으로 주문을 완료하였습니다.", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    companion object {
+        fun newInstance(context: Context) = Intent(context, OrderMenuListActivity::class.java)
+    }
+
+}
