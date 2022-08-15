3d14b6575febe8d03080ecd977d419a282061e77
daon1213 <diaeiyo1213@naver.com>
2022-08-15 19:57:13 +0900

    🔧 - 주문 성공 및 주문 오류에 대한 처리

---
+++ app/src/main/java/com/daon/goj_gam/screen/order/OrderMenuState.kt
@@ -0,0 +1,23 @@
+package com.daon.goj_gam.screen.order
+
+import androidx.annotation.StringRes
+import com.daon.goj_gam.model.food.FoodModel
+
+sealed interface OrderMenuState{
+
+    object UnInitialized: OrderMenuState
+
+    object Loading: OrderMenuState
+
+    data class Success(
+        val restaurantFoodModelIst: List<FoodModel>? = null
+    ): OrderMenuState
+
+    object Order: OrderMenuState
+
+    data class Error(
+        @StringRes val messageId: Int,
+        val e : Throwable
+    ): OrderMenuState
+
+}
