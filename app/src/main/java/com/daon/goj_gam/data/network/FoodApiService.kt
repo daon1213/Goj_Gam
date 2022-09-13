64866321ba10156221e6e40c73834bbef9b5eff3
daon1213 <diaeiyo1213@naver.com>
2022-09-13 11:54:46 +0900

    🔧 - 음식 정보 가져오기

---
+++ app/src/main/java/com/daon/goj_gam/data/network/FoodApiService.kt
@@ -0,0 +1,15 @@
+package com.daon.goj_gam.data.network
+
+import com.daon.goj_gam.data.response.restaurant.RestaurantFoodResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FoodApiService {
+
+    @GET("restaurants/{restaurantId}/foods")
+    suspend fun getRestaurantFoods(
+        @Path("restaurantId") restaurantId: Long
+    ): Response<List<RestaurantFoodResponse>>
+
+}
