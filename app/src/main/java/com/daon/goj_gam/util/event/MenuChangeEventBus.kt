0a14712908a5c236aded4e28351c768344c49172
daon1213 <diaeiyo1213@naver.com>
2022-09-19 14:44:51 +0900

    🔧 - 장바구니 메뉴 교체 로직

---
+++ app/src/main/java/com/daon/goj_gam/util/event/MenuChangeEventBus.kt
@@ -0,0 +1,14 @@
+package com.daon.goj_gam.util.event
+
+import com.daon.goj_gam.screen.main.MainTabMenu
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+class MenuChangeEventBus {
+
+    val mainTabMenuFlow = MutableSharedFlow<MainTabMenu>()
+
+    suspend fun changeMenu(menu: MainTabMenu) {
+        mainTabMenuFlow.emit(menu)
+    }
+
+}
