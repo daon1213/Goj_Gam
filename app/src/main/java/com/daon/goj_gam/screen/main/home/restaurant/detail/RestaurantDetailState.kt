71cefa1383dd29afad683e63427e4bbb227bb38d
daon1213 <diaeiyo1213@naver.com>
2022-09-25 16:29:34 +0900

    🔧 - 식당 상세 정보 가져오기

---
+++ app/src/main/java/com/daon/goj_gam/screen/main/home/restaurant/detail/RestaurantDetailState.kt
@@ -0,0 +1,20 @@
+package com.daon.goj_gam.screen.main.home.restaurant.detail
+
+import com.daon.goj_gam.data.entity.impl.RestaurantEntity
+import com.daon.goj_gam.data.entity.impl.RestaurantFoodEntity
+
+sealed class RestaurantDetailState {
+
+    object Uninitialized: RestaurantDetailState()
+
+    object Loading: RestaurantDetailState()
+
+    data class Success(
+        val restaurantEntity: RestaurantEntity,
+        val restaurantFoodList: List<RestaurantFoodEntity>? = null,
+        val foodMenuListInBasket: List<RestaurantFoodEntity>? = null,
+        val isClearNeedInBasketAndAction: Pair<Boolean, () -> Unit> = Pair(false, {}),
+        val isLiked: Boolean? = null
+    ): RestaurantDetailState()
+
+}
